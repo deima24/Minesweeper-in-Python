a45c47707e43addae1e27d045571d705bb4eb99c
deima24 <12deima24@gmail.com>
2022-12-04 23:57:54 +0000

    Created a plan what i need to make in a project

---
+++ run.py
@@ -0,0 +1,12 @@
+#play the game
+def play(dim_size = 10, num_bombs = 10):
+    
+
+
+    # Step 1: Create the board and plant the bombs
+    # Step 2: Show the user the board and ask for where they want to dig
+    # Step 3: If location is a bomb, show game over message
+    # Step 4: If location is not a bomb, dig recursively until each square is at
+    #   next to a bomb 
+    # Step 5: Repeat step 2, 3 and 4 until there are no more places to dig(You win)
+    
